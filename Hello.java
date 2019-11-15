2c8ed241b294c828118800ae7e50797092446a15
Christopher <ransomc1@go.stockton.edu>
2019-11-15 10:20:49 -0500

    Added a default value

--- Hello.java
+++ Hello.java
@@ -1,7 +1,10 @@
 
 public class Hello {
 	public static void main(String argv[]) {
-System.out.println("Hello, " + argv[0] + "!");
+		
+		String Name = "World";
+		if (argv.length != 0)name=argv[0];
+		System.out.println("Hello, " + argv[0] + "!");
 }
 
 
