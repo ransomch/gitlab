8f80d4fa22da679f4f5ae88408ed13370eb16775
Christopher <ransomc1@go.stockton.edu>
2019-11-15 10:25:55 -0500

    B Revert "Oops, we didn't want this commit"
    This reverts commit 07723d2097cb84e70301ccb75bf6002346e4d205.

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
 	public static void main(String argv[]) {
-	        //This is an unwanted but staged comment
+		
 		String Name = "World";
 		if (argv.length != 0)name=argv[0];
 		System.out.println("Hello, " + argv[0] + "!");
