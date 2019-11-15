411ccea5f34058a2a547852a4da79ea94ad44bb8
Christopher <ransomc1@go.stockton.edu>
2019-11-15 10:17:04 -0500

    Second Message

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 
 public class Hello {
-	public static void main(String[] args) {
-System.out.println("Hello World");
+	public static void main(String argv[]) {
+System.out.println("Hello, " + argv[0] + "!");
 }
-}
+
+
+
+
+
